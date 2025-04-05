5e36fbf8a25c46e779113460b9a12ecf80653500
PepijnBullens <pepijnpersoonlijk@gmail.com>
2025-04-05 16:52:35 +0200

    Fixed reponsiveness bug

--- next-app/src/layouts/main-layout.tsx
+++ next-app/src/layouts/main-layout.tsx
@@ -16,7 +16,7 @@ export default function MainLayout({
       <div className="z-0 flex fixed bottom-0 left-0 w-[42rem] md:opacity-100 opacity-80 translate-y-1/3 -translate-x-1/3 aspect-square bg-[#69DC9E] rounded-full"></div>
       <div className="z-0 flex fixed bottom-0 right-0 w-[38rem] md:opacity-100 opacity-90 translate-y-2/5 translate-x-2/7 aspect-square bg-[#E8C547] rounded-full"></div>
 
-      <div className="w-full min-h-full p-4 md:p-8 pt-32 md:pt-32 z-100 flex gap-6 md:flex-row justify-between flex-col">
+      <div className="w-full min-h-full p-4 md:p-8 pt-32 md:pt-32 z-100 flex gap-6 md:flex-row justify-between md:justify-start flex-col">
         {children}
       </div>
     </div>
